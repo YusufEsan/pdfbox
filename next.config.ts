b0b252605be94934ef76fbf46311a635e5ca9d62
Yusuf Esan <yusufesan@Yusuf-MacBook-Pro.local>
2026-03-11 16:32:10 +0300

    feat: add GitHub Pages deployment with static export and GitHub Actions workflow

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  //devIndicators: false,
+  output: 'export',
+  basePath: isProd ? '/pdfbox' : '',
+  assetPrefix: isProd ? '/pdfbox/' : '',
+  devIndicators: false,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
